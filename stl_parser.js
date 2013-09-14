ee3adb80ca20b722683f9e72f6767a012a409444
Steven Zeiler <zeiler.steven@gmail.com>
2013-09-13 21:11:59 -0700

    [CHORE] Focus on hand and zoom out.

--- stl_parser.js
+++ stl_parser.js
@@ -1,5 +1,21 @@
-var camera, scene, renderer,
-geometry, material, mesh, light1, stats;
+function fetchStl (url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if ( xhr.readyState == 4 ) {
+      if ( xhr.status == 200 || xhr.status == 0 ) {
+        var rep = xhr.response;
+        callback(rep);
+      }
+    }
+  }
+  xhr.onerror = function(e) {
+      console.log(e);
+  }
+
+  xhr.open( "GET", url, true );
+  xhr.responseType = "arraybuffer";
+  xhr.send( null );
+}
 
 function trim (str) {
     str = str.replace(/^\s+/, '');
@@ -11,6 +27,11 @@ function trim (str) {
     }
     return str;
 }
+
+var camera, scene, renderer,
+geometry, material, mesh, stats, directionalLight;
+
+
 
 // Notes:
 // - STL file format: http://en.wikipedia.org/wiki/STL_(file_format)
@@ -67,118 +88,6 @@ var parseStlBinary = function(stl) {
     return geo;
 };  
 
-var parseStl = function(stl) {
-    var state = '';
-    var lines = stl.split('\n');
-    var geo = new THREE.Geometry();
-    var name, parts, line, normal, done, vertices = [];
-    var vCount = 0;
-    stl = null;
-
-    for (var len = lines.length, i = 0; i < len; i++) {
-        if (done) {
-            break;
-        }
-        line = trim(lines[i]);
-        parts = line.split(' ');
-        switch (state) {
-            case '':
-                if (parts[0] !== 'solid') {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "solid"');
-                    return;
-                } else {
-                    name = parts[1];
-                    state = 'solid';
-                }
-                break;
-            case 'solid':
-                if (parts[0] !== 'facet' || parts[1] !== 'normal') {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "facet normal"');
-                    return;
-                } else {
-                    normal = [
-                        parseFloat(parts[2]), 
-                        parseFloat(parts[3]), 
-                        parseFloat(parts[4])
-                    ];
-                    state = 'facet normal';
-                }
-                break;
-            case 'facet normal':
-                if (parts[0] !== 'outer' || parts[1] !== 'loop') {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "outer loop"');
-                    return;
-                } else {
-                    state = 'vertex';
-                }
-                break;
-            case 'vertex': 
-                if (parts[0] === 'vertex') {
-                    geo.vertices.push(new THREE.Vector3(
-                        parseFloat(parts[1]),
-                        parseFloat(parts[2]),
-                        parseFloat(parts[3])
-                    ));
-                } else if (parts[0] === 'endloop') {
-                    geo.faces.push( new THREE.Face3( vCount*3, vCount*3+1, vCount*3+2, new THREE.Vector3(normal[0], normal[1], normal[2]) ) );
-                    vCount++;
-                    state = 'endloop';
-                } else {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "vertex" or "endloop"');
-                    return;
-                }
-                break;
-            case 'endloop':
-                if (parts[0] !== 'endfacet') {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "endfacet"');
-                    return;
-                } else {
-                    state = 'endfacet';
-                }
-                break;
-            case 'endfacet':
-                if (parts[0] === 'endsolid') {
-                    //mesh = new THREE.Mesh( geo, new THREE.MeshNormalMaterial({overdraw:true}));
-                    mesh = new THREE.Mesh( 
-                        geo, 
-                        new THREE.MeshLambertMaterial({
-                            overdraw:true,
-                            color: 0xaa0000,
-                            shading: THREE.FlatShading
-                        }
-                    ));
-                    scene.add(mesh);
-                    done = true;
-                } else if (parts[0] === 'facet' && parts[1] === 'normal') {
-                    normal = [
-                        parseFloat(parts[2]), 
-                        parseFloat(parts[3]), 
-                        parseFloat(parts[4])
-                    ];
-                    if (vCount % 1000 === 0) {
-                        console.log(normal);
-                    }
-                    state = 'facet normal';
-                } else {
-                    console.error(line);
-                    console.error('Invalid state "' + parts[0] + '", should be "endsolid" or "facet normal"');
-                    return;
-                }
-                break;
-            default:
-                console.error('Invalid state "' + state + '"');
-                break;
-        }
-    }
-};
-
-
-
 init();
 animate();
 
@@ -192,35 +101,16 @@ function init() {
     camera.position.y = 0;
     scene.add( camera );
 
-    var directionalLight = new THREE.DirectionalLight( 0xffffff );
+    directionalLight = new THREE.DirectionalLight( 0xffffff );
     directionalLight.position.x = 0; 
     directionalLight.position.y = 0; 
     directionalLight.position.z = 1; 
     directionalLight.position.normalize();
     scene.add( directionalLight );
 
-    function fetchStl (url, callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if ( xhr.readyState == 4 ) {
-          if ( xhr.status == 200 || xhr.status == 0 ) {
-            var rep = xhr.response;
-            callback(rep);
-          }
-        }
-      }
-      xhr.onerror = function(e) {
-          console.log(e);
-      }
-    
-      xhr.open( "GET", url, true );
-      xhr.responseType = "arraybuffer";
-      xhr.send( null );
-    }
-
     fetchStl('hand_ok.stl', function (stlBinary) {
       var geometry = parseStlBinary(stlBinary);
-      var mesh = new THREE.Mesh(geometry,
+      mesh = new THREE.Mesh(geometry,
         // new THREE.MeshNormalMaterial({
         //     overdraw:true
         // }
@@ -249,25 +139,25 @@ function init() {
 }
 
 function animate() {
-
     // note: three.js includes requestAnimationFrame shim
     requestAnimationFrame( animate );
     render();
     stats.update();
-
 }
+
 
 function render() {
 
     //mesh.rotation.x += 0.01;
+
+    mesh.position.y = -50;
+    mesh.position.z = -150;
+    
     if (mesh) {
         mesh.rotation.z += 0.01;
-        mesh.rotation.x += 0.02;
-        mesh.rotation.y += 0.03;
-
+        //mesh.rotation.x += 0.02;
+        //mesh.rotation.y += 0.03;
     }
-    //light1.position.z -= 1;
-
     renderer.render( scene, camera );
 
 }
